9f4cff7a6c2702b9f259fc28882b0f4f3f93965a
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-11-09 22:22:55 +0700

    safdsa

---
+++ RefferalLinksBackEnd/RefferalLinks.DAL/Implementation/LinkTempalteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maynghien.Common.Repository;
+using RefferalLinks.DAL.Models.Context;
+using RefferalLinks.DAL.Models.Entity;
+
+namespace RefferalLinks.DAL.Implementation
+{
+    public class LinkTempalteRepository : GenericRepository<LinkTemplate, RefferalLinksDbContext, ApplicationUser>
+    {
+        public LinkTempalteRepository(RefferalLinksDbContext unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
