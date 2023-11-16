9fc0a29e26cbd2b3700e879edcd7f337a3520042
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-11-16 14:00:24 +0700

    xA

--- RefferalLinksBackEnd/RefferalLinks.DAL/Models/Entity/Team.cs
+++ RefferalLinksBackEnd/RefferalLinks.DAL/Models/Entity/Team.cs
@@ -1,14 +1,8 @@
 ﻿using MayNghien.Common.Models.Entity;
-using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RefferalLinks.DAL.Models.Entity
 {
-  public class Team : BaseEntity
+    public class Team : BaseEntity
     {
         public string name { get; set; }
     }
