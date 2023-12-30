921105a33caff4d99e4ba81b3a870389f937ae33
Phan Thanh Binh <maynghien@hotmail.com>
2023-12-30 21:20:38 +0700

    dfdsf

--- RefferalLinksBackEnd/RefferalLinks.API/Program.cs
+++ RefferalLinksBackEnd/RefferalLinks.API/Program.cs
@@ -86,11 +86,11 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors(builder =>
